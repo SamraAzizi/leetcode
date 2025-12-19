b8852b224aeeaaf27bb84a54491fecd88da4b654
SamraAzizi <samarraazizi@gmail.com>
2025-12-19 22:08:28 +0500

    medium leetcode

---
+++ intermediate/add-two-number.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy_head = ListNode(0)
+        current = dummy_head
+        carry = 0
+        
+        while l1 is not None or l2 is not None or carry:
+            val1 = l1.val if l1 is not None else 0
+            val2 = l2.val if l2 is not None else 0
+            
+            total = val1 + val2 + carry
+            carry = total // 10
+            new_digit = total % 10
+            
+            current.next = ListNode(new_digit)
+            current = current.next
+            
+            if l1 is not None:
+                l1 = l1.next
+            if l2 is not None:
+                l2 = l2.next
+        
+        return dummy_head.nex
+    
+solution = Solution()
+print(solution.addTwoNumbers([2,4,3], [5,6,4]))
