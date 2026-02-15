558ff634ec1b1fbf6483e8962c3dda899f131e1e
SamraAzizi <samarraazizi@gmail.com>
2026-02-15 19:10:09 +0500

    intermediate leetcode

---
+++ easy/addBinary.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        i, j = len(a) - 1, len(b) - 1
+        carry = 0
+        result = []
+        while i >= 0 or j >= 0 or carry:
+            sum = carry
+            if i >= 0:
+                sum += int(a[i])
+                i -= 1
+            if j >= 0:
+                sum += int(b[j])
+                j -= 1
+            result.append(str(sum % 2))
+            carry = sum // 2
+        return ''.join(reversed(result))
